1060145f9b9fd106ba23b65beffd293c491b293b
Peyman <143264765+peymanfr71@users.noreply.github.com>
2023-08-29 11:37:57 +0330

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+#split([sep[, maxsplit]]):
+text = 'hola me amor lana der rey'
+result = text.split('a')
+#print (result)
+
+text = 'hola me amor lana der rey'
+wool = text.split()
+result = '*'.join(wool)
+print (result)
+
+text = 'hola me amor lana der rey'
+words = text.split()
+result = '*'.join(words)
+#print(result)
