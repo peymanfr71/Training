33dc210e7163d8a89dbcf0b19b8f71205a0db2db
Peyman <143264765+peymanfr71@users.noreply.github.com>
2023-08-29 11:38:51 +0330

    Add files via upload

---
+++ Untitled-3.py
@@ -0,0 +1,9 @@
+name = input ('please give me your name: ')
+
+if name == 'peyman':
+    print("hi bro, whasap?")
+elif name == 'lana':
+    print('oh, my Goddess')
+else:
+    print("Hola extraño")
+    
